f1f5c20d1c60fa3e7b8a0f0bdd2ffcd217310ea1
Stanislav M <phoenix0597@yandex.ru>
2025-01-08 00:21:30 +0300

    Fix app/models/activities.py

---
+++ app/models/activities.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.db.base import Base
+
+
+class Activity(Base):
+    __tablename__ = "activities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    parent_id = Column(Integer, ForeignKey("activities.id"), nullable=True, index=True)
+    level = Column(Integer, nullable=False, default=1)
+
+    children = relationship(
+        "Activity", back_populates="parent", cascade="all, delete-orphan"
+    )
+
+    parent = relationship(
+        "Activity", back_populates="children", remote_side=[id], uselist=False
+    )
+    relationship(
+        "Organization",
+        secondary="organizations_activities",
+        back_populates="activities",
+    )
+
+
+# проверить этот класс на корректность!!!
