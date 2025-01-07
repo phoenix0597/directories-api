9662788b2ccd9641e676e7058d41b039ff672382
Stanislav M <phoenix0597@yandex.ru>
2025-01-07 21:21:59 +0300

    Add app/models/organizations.py

---
+++ app/models/organizations.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+from app.db.base import Base
+
+
+organizations_activities = Table(
+    "organizations_activities",
+    Base.metadata,
+    Column("organization_id", Integer, ForeignKey("organizations.id")),
+    Column("activity_id", Integer, ForeignKey("activities.id"), primary_key=True),
+)
+
+
+class Organization(Base):
+    __tablename__ = "organizations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    building_id = Column(Integer, ForeignKey("buildings.id"), nullable=False)
+    building = relationship("Building", back_populates="organizations")
+    phones = relationship(
+        "Phone", back_populates="organization", cascade="all, delete-orphan"
+    )
+    activities = relationship("Activity", secondary="organizations_activities")
